e5e2dda7e14da6fdc5b1efb858b7c221f798c9b4
josinijose06 <105633741+josinijose06@users.noreply.github.com>
2022-08-01 11:58:10 +0530

    Add files via upload

---
+++ Skyscraper/src/com/gl/driver/Skyscraper.java
@@ -0,0 +1,36 @@
+package com.gl.driver;
+import java.util.Scanner;
+
+public class Skyscraper {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter the total number floors in the building : ") ;
+	   	Scanner sc = new Scanner(System.in);
+	        int d = sc.nextInt();
+	        int f[] = new int[d+1];
+	        for(int i=1;i<d+1;i++){
+	System.out.println("Enter the floor size given on day : "+i);
+	        int s=sc.nextInt();
+	        f[s]=i;	}
+	        int j=d;
+	        boolean flag;
+	System.out.println("The order of construction is as follows ");
+	        for(int i=1;i<=d;i++){
+	        	
+	        flag=false;
+	System.out.println("Day : "+i);		
+	        while(j>=1 && f[j]<=i){
+	        flag=true;
+	 System.out.print(j +" ");
+	        j--; }
+	        if(flag==true){
+	 System.out.println(" ");
+	 sc.close();
+            }
+        }	
+		
+
+	}
+
+}
